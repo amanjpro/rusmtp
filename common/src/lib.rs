c1e9346feb62b6aa0fb7cf3f7c3fb9aee3809ba7
amanjpro <amanj.sherwany@usi.ch>
2018-02-26 21:03:01 -0500

    Support the new structure for smtprc files

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -30,80 +30,97 @@ const DEFAULT_HEARTBEAT_IN_MINUTES: u8 = 3;
 const DEFAULT_TIMEOUT_IN_SECONDS: u64 = 30;
 
 #[derive(Debug)]
-pub struct Configuration {
-    pub passwordeval: String,
-    pub smtpclient: Option<String>,
+pub struct Account {
+    pub label: String,
     pub username: Option<String>,
+    pub passwordeval: String,
     pub host: Option<String>,
     pub port: Option<u16>,
     pub tls: Option<bool>,
     pub heartbeat: u8,
+    pub default: bool,
+}
+
+#[derive(Debug)]
+pub struct Configuration {
+    pub smtpclient: Option<String>,
     pub timeout: u64,
+    pub accounts: Vec<Account>,
 }
 
 pub fn read_config(rc_path: &str) -> Configuration {
     let conf = Ini::load_from_file(rc_path).unwrap();
 
-    let section = conf.section(Some("Daemon".to_owned()))
-        .expect("SMTP Daemon section is missing in the configuration");
+    let smtp = conf.section(Some("Daemon".to_owned())).and_then(|section| {
+        section.get("smtp").map(|s| s.to_string())
+    });
 
-    let eval = section.get("passwordeval")
-        .expect("passwordeval is missing in the configuration");
-    let smtp = section.get("smtp").map(|s| s.to_string());
-
-    let timeout = conf.section(Some("Client")).map(|section| {
+    let timeout = conf.section(Some("Client")).and_then(|section| {
         section.get("timeout").map(|s| {
             let res: u64 = s.parse()
                 .expect("Invalid timeout value in configuration");
             res
-        }).unwrap_or(DEFAULT_TIMEOUT_IN_SECONDS)
+        })
     }).unwrap_or(DEFAULT_TIMEOUT_IN_SECONDS);
 
-    match conf.section(Some("SMTP".to_owned())) {
-        Some(section)     => {
-                let username = section.get("username").map(|s| s.to_string());
-                let host     = section.get("host").map(|s| s.to_string());
-                let port     = section.get("port").map(|p| {
-                    let port: u16 = p.parse()
-                        .expect("Invalid port number value in configuration");
-                    port
-                });
 
-                let tls      = section.get("tls").map(|p| {
-                    let tls: bool = p.parse()
-                        .expect("Invalid tls value in configuration (valid: false | true)");
-                    tls
-                });
+    let mut accounts: Vec<Account> = Vec::new();
 
-                let heartbeat      = section.get("heartbeat").map(|p| {
-                    let heartbeat: u8 = p.parse()
-                        .expect("Invalid u8 value in configuration");
-                    heartbeat
-                }).unwrap_or(DEFAULT_HEARTBEAT_IN_MINUTES);
+    for (section_name, section) in conf.iter() {
+        if *section_name != Some("Client".to_string()) &&
+                *section_name != Some("Daemon".to_string()) {
+            let label = section_name.clone().unwrap();
+            let host     = section.get("host").map(|s| s.to_string());
+            let username = section.get("username").map(|s| s.to_string());
+            let port     = section.get("port").map(|p| {
+                let port: u16 = p.parse()
+                    .expect("Invalid port number value in configuration");
+                port
+            });
+            let eval     = section.get("passwordeval").map(|s| s.to_string())
+                .expect("passwordeval is missing in the configuration");
+
+            let tls      = section.get("tls").map(|p| {
+                let tls: bool = p.parse()
+                    .expect("Invalid tls value in configuration (valid: false | true)");
+                tls
+            });
+
+            let heartbeat      = section.get("heartbeat").map(|p| {
+                let heartbeat: u8 = p.parse()
+                    .expect("Invalid u8 value in configuration");
+                heartbeat
+            }).unwrap_or(DEFAULT_HEARTBEAT_IN_MINUTES);
+
+            let default      = section.get("default").map(|p| {
+                let default: bool = p.parse()
+                    .expect("Invalid bool value in configuration");
+                default
+            }).unwrap_or(false);
 
 
-                Configuration {
-                    passwordeval: eval.to_string(),
-                    smtpclient: smtp,
-                    username: username,
-                    host: host,
-                    port: port,
-                    tls: tls,
-                    heartbeat: heartbeat,
-                    timeout: timeout,
-                }
-            },
-        None             =>
-            Configuration {
+
+            accounts.push(Account {
+                label: label,
+                host: host,
+                username: username,
                 passwordeval: eval.to_string(),
-                smtpclient: smtp,
-                username: None,
-                host: None,
-                port: None,
-                tls: None,
-                heartbeat: DEFAULT_HEARTBEAT_IN_MINUTES,
-                timeout: timeout,
-            },
+                port: port,
+                tls: tls,
+                heartbeat: heartbeat,
+                default: default,
+            })
+        }
+    }
+
+    if accounts.is_empty() {
+        panic!("At least an account should be configured");
+    }
+
+    Configuration {
+        smtpclient: smtp,
+        timeout: timeout,
+        accounts: accounts,
     }
 }
 
