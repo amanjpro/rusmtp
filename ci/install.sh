990b147b67fe1fb001e06cca8a575b86a6e02c4f
amanjpro <amanjpro@gmail.com>
2018-10-16 23:23:50 -0400

    Simplify Travis build description and add test to it

--- ci/install.sh
+++ ci/install.sh
@@ -3,33 +3,23 @@
 set -o errexit -o nounset -o pipefail
 
 sudo apt-get update
-sudo apt-get install -y clang-3.4
 sudo apt-get install -qq gcc-arm-linux-gnueabihf
 sudo apt-get install -y pkg-config libssl-dev
-sudo apt-get update
+sudo apt-get install -y libc6-armhf-cross
+sudo apt-get install -y libc6-dev-armhf-cross
+sudo apt-get install -y qemu-user-static
+sudo apt-get install -y binfmt-support
 
 
 # Add targets to rustup
-rustup target add x86_64-apple-darwin
 rustup target add armv7-unknown-linux-gnueabihf
 
 
-# Install OSX cross device tools
-git clone https://github.com/tpoechtrager/osxcross
-cd osxcross
-wget https://s3.dockerproject.org/darwin/v2/MacOSX10.11.sdk.tar.xz
-mv MacOSX10.11.sdk.tar.xz tarballs/
-sed -i -e 's|-march=native||g' build_clang.sh wrapper/build.sh
-UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
-sudo mkdir -p /usr/local/osx-ndk-x86
-sudo mv target/* /usr/local/osx-ndk-x86
-cd ..
-rm -rf osxcross
-
+current_directory=$(pwd)
 
 # Install ARM openssl
-current_directory=$(pwd)
-cd /tmp
+mkdir -p "$HOME/openssl-1.1.1-armv7"
+cd "$HOME/openssl-1.1.1-armv7"
 wget https://www.openssl.org/source/openssl-1.1.1.tar.gz
 tar xzf openssl-1.1.1.tar.gz
 export MACHINE=armv7
@@ -38,11 +28,7 @@ export CC=arm-linux-gnueabihf-gcc
 cd openssl-1.1.1 && ./config shared && make && cd -
 cd "$current_directory"
 
-cat >$HOME/.cargo/config <<EOF
-[target.x86_64-apple-darwin]
-linker = "x86_64-apple-darwin15-cc"
-ar = "x86_64-apple-darwin15-ar"
-
+cat >"$HOME/.cargo/config" <<EOF
 [target.armv7-unknown-linux-gnueabihf]
 linker = "arm-linux-gnueabihf-gcc"
 EOF
