2b8289fb4ed7af1d71ea996bac16eedcf2342999
amanjpro <amanjpro@gmail.com>
2018-10-24 23:31:35 -0400

    Add locking, to have single client per socket at a time, fixes #28

--- main/src/rusmtpc.rs
+++ main/src/rusmtpc.rs
@@ -1,15 +1,19 @@
+extern crate fs2;
 extern crate common;
 
 
+use std::fs::File;
+use std::{thread, time};
+use std::io::{self, Read, Write};
 use std::os::unix::net::UnixStream;
 use std::process::exit;
 use std::net::Shutdown;
 use std::time::Duration;
+use fs2::FileExt;
 use common::*;
 use common::args::*;
 use common::mail::*;
 use common::config::*;
-use std::io::{self, Read, Write};
 
 fn main () {
 
@@ -36,6 +40,11 @@ fn main () {
       value
     });
 
+    let lock_file = File::open(get_lock_path(account)).unwrap();
+    let ten_millis = time::Duration::from_millis(10);
+    while lock_file.lock_exclusive().is_err() {
+        thread::sleep(ten_millis);
+    }
     let mut stream = UnixStream::connect(get_socket_path(account))
         .expect("The daemon is not running, please start it.");
     stream.write_all(mail.serialize().as_slice()).unwrap();
@@ -45,6 +54,7 @@ fn main () {
     let mut response = Vec::new();
     let _ = stream.read_to_end(&mut response).expect("Timeout is met, please retry");
     let response = String::from_utf8(response).expect("Cannot decode the response");
+    let _ = lock_file.unlock();
     if ERROR_SIGNAL == response { exit(1); }
     else if OK_SIGNAL == response { exit(0); }
     else {
