343c3425042c33c9f31788502e7f37d8129081a9
amanjpro <amanjpro@gmail.com>
2018-11-14 01:11:39 -0500

    Add env

--- ci/script_linux.sh
+++ ci/script_linux.sh
@@ -6,5 +6,6 @@ cargo test --target=x86_64-unknown-linux-gnu
 cargo build --target=x86_64-unknown-linux-gnu --release
 # export OPENSSL_LIB_DIR=$HOME/openssl-1.1.1-armv7/openssl-1.1.1
 # export OPENSSL_INCLUDE_DIR=$HOME/openssl-1.1.1-armv7/openssl-1.1.1/include
+export PKG_CONFIG_ALLOW_CROSS=1
 cargo build --target=armv7-unknown-linux-gnueabihf --release
 
