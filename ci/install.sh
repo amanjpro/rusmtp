29c9dd93b8d7e4e19febc66e611034ae6765a6d2
amanjpro <amanjpro@gmail.com>
2018-11-14 00:58:15 -0500

    Do not build openssl for armv7, try vendored

--- ci/install.sh
+++ ci/install.sh
@@ -10,18 +10,18 @@ sudo apt-get install -y pkg-config libssl-dev
 rustup target add armv7-unknown-linux-gnueabihf
 
 
-current_directory=$(pwd)
+#current_directory=$(pwd)
 
 # Install ARM openssl
-mkdir -p "$HOME/openssl-1.1.1-armv7"
-cd "$HOME/openssl-1.1.1-armv7"
-wget https://www.openssl.org/source/openssl-1.1.1.tar.gz
-tar xzf openssl-1.1.1.tar.gz
-export MACHINE=armv7
-export ARCH=arm
-export CC=arm-linux-gnueabihf-gcc
-cd openssl-1.1.1 && ./config shared && make
-cd "$current_directory"
+# mkdir -p "$HOME/openssl-1.1.1-armv7"
+# cd "$HOME/openssl-1.1.1-armv7"
+# wget https://www.openssl.org/source/openssl-1.1.1.tar.gz
+# tar xzf openssl-1.1.1.tar.gz
+# export MACHINE=armv7
+# export ARCH=arm
+# export CC=arm-linux-gnueabihf-gcc
+# cd openssl-1.1.1 && ./config shared && make
+# cd "$current_directory"
 
 cat >"$HOME/.cargo/config" <<EOF
 [target.armv7-unknown-linux-gnueabihf]
--- ci/script_linux.sh
+++ ci/script_linux.sh
@@ -4,7 +4,7 @@ set -o errexit -o nounset -o pipefail
 
 cargo test --target=x86_64-unknown-linux-gnu
 cargo build --target=x86_64-unknown-linux-gnu --release
-export OPENSSL_LIB_DIR=$HOME/openssl-1.1.1-armv7/openssl-1.1.1
-export OPENSSL_INCLUDE_DIR=$HOME/openssl-1.1.1-armv7/openssl-1.1.1/include
+# export OPENSSL_LIB_DIR=$HOME/openssl-1.1.1-armv7/openssl-1.1.1
+# export OPENSSL_INCLUDE_DIR=$HOME/openssl-1.1.1-armv7/openssl-1.1.1/include
 cargo build --target=armv7-unknown-linux-gnueabihf --release
 
